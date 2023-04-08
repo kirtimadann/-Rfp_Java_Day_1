f520bd71cc97ad2eefdc923c0cc5b84fd8c2e1db
lunainfotec <kirtimadan1997@gmail.com>
2023-04-08 19:34:51 +0530

    added : comment side in Qusions

---
+++ src/com/bl/day1/DefaultValuePrimitivveType.java
@@ -0,0 +1,28 @@
+package com.bl.day1;
+
+public class DefaultValuePrimitivveType {
+
+    static  int i;
+    static  char c;
+    static float f;
+    static double d;
+    static byte b;
+    static boolean bl;
+    static short s;
+    static long l;
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("int :" + i);
+        System.out.println("char :" + c);
+        System.out.println("byte :" + b);
+        System.out.println("double :" + d);
+        System.out.println("short :" + s);
+        System.out.println("long :" + l);
+        System.out.println("boolean :" +bl);
+        System.out.println("float :" + f);
+
+    }
+}
--- src/com/bl/day1/FirstMessage.java
+++ src/com/bl/day1/FirstMessage.java
@@ -1,5 +1,5 @@
 package com.bl.day1;
-
+// Write a program to display any message:
 public class FirstMessage {
 
     public static void main(String[] args) {
