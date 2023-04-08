3b3b2bcdf57711df68e28efa161d6f2b2d370418
lunainfotec <kirtimadan1997@gmail.com>
2023-04-08 19:15:01 +0530

    added : normal meassge first

---
+++ src/com/bl/day1.java
@@ -0,0 +1,11 @@
+package com.bl;
+
+public class day1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi am kirti i do practice for best full stack developer");
+
+    }
+
+}
