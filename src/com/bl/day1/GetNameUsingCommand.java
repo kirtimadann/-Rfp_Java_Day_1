8632afdfc7b971738875f5bd03ffaa14b81dbc44
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 16:29:58 +0530

    get name using command line

---
+++ src/com/bl/day1/GetNameUsingCommand.java
@@ -0,0 +1,10 @@
+package com.bl.day1;
+import java.util.*;
+public class GetNameUsingCommand {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+          String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
