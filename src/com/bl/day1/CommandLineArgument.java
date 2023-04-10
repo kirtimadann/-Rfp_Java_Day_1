2b1bb238b828116bbf9cd033cdd91c0c3b449ed3
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 16:07:46 +0530

    added QA4

--- src/com/bl/day1/CommandLineArgument.java
+++ src/com/bl/day1/CommandLineArgument.java
@@ -5,9 +5,6 @@ public class CommandLineArgument {
     public static void main(String[] args) {
 
         try{
-
-
-
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b= sc.nextInt();
